5444176d4988d2bb6a1e6d3a659bebf0270cf3ee
f1uk3r <agarwal.umang4321@gmail.com>
2020-05-10 20:40:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    myLibrary.push(aBook)
+}
+
