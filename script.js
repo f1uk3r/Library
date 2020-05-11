e99bc69561abd02b32bd323112d325e08ba4e8f1
f1uk3r <agarwal.umang4321@gmail.com>
2020-05-11 16:02:31 +0530

    function insertRowInTable working

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function addBookToLibrary() {
     let title = document.querySelector('#title').value
     let author = document.querySelector('#author').value
     let pages = document.querySelector('#pages').value
-    let read = document.querySelector('#read').value
+    let read = document.querySelector('#read').checked
 
     let aBook = new Book(title, author, pages, read)
     myLibrary.push(aBook)
-    
-
+    console.log(myLibrary)
+    insertRowInTable(aBook)
 }
 
 const form = document.querySelector('form')
@@ -25,3 +25,30 @@ form.addEventListener('submit', event => {
     addBookToLibrary();
 })
 
+function insertRowInTable(bookObject) {
+    let tableBody = document.querySelector('tBody')
+    let tr = document.createElement('tr')
+    let titleTd = document.createElement('td')
+    let titleText = document.createTextNode(bookObject.name);
+    titleTd.appendChild(titleText)
+    tr.appendChild(titleTd)
+    let authorTd = document.createElement('td')
+    let authorText = document.createTextNode(bookObject.author);
+    authorTd.appendChild(authorText)
+    tr.appendChild(authorTd)
+    let pagesTd = document.createElement('td')
+    let pagesText = document.createTextNode(bookObject.pages);
+    pagesTd.appendChild(pagesText)
+    tr.appendChild(pagesTd)
+    let readTd = document.createElement('td')
+    let readText = document.createTextNode(bookObject.read);
+    readTd.appendChild(readText)
+    tr.appendChild(readTd)
+    let deleteTd = document.createElement('td')
+    let deleteButton = document.createElement('button')
+    let deleteText = document.createTextNode('Delete');
+    deleteButton.appendChild(deleteText)
+    deleteTd.appendChild(deleteButton)
+    tr.appendChild(deleteTd);
+    tableBody.appendChild(tr);
+}
