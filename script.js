e718676020c361bbe55f58d76c398520735be1b2
f1uk3r <agarwal.umang4321@gmail.com>
2020-05-11 14:54:48 +0530

    book form submit working

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary() {
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#pages').value
+    let read = document.querySelector('#read').value
+
+    let aBook = new Book(title, author, pages, read)
     myLibrary.push(aBook)
+    
+
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    addBookToLibrary();
+})
 
